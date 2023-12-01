b7f9f7d70552ae880792b6b216fe88460b12a12b
kueihua <kueihua.chang@gmail.com>
2023-12-01 13:50:30 +0800

    Create class_Inheritance.cpp

---
+++ class_Inheritance.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+class IFilter {
+  public:
+    IFilter(){};
+    IFilter(int in);
+    virtual ~IFilter();
+    virtual void start() = 0;
+  private:
+    int type;
+};
+IFilter::IFilter(int in) {
+    std::cout << "IFilter(IIII) \n";
+    type = in;
+}
+IFilter::~IFilter() {
+    std::cout << "~IFilter(IIII) \n";
+}
+
+
+class Filter : public IFilter {
+  public:
+    Filter(){};
+    Filter(int in);
+    virtual ~Filter();
+    virtual void start() override;
+  private:
+    int type;
+};
+Filter::Filter(int in) {
+    std::cout << "Filter(D111) \n";
+    type = in;
+}
+Filter::~Filter() {
+    std::cout << "~Filter(D111) \n";
+}
+void Filter::start() {
+    std::cout << "start(D111):" << "type=" << type <<"\n";
+}
+
+class AFilter: public Filter {
+  public:
+    AFilter(){};
+    AFilter(int in);
+    virtual void start() override;
+  private:
+    ~AFilter();
+    int type;    
+};
+AFilter::AFilter(int in) {
+    std::cout << "AFilter(D222) \n";
+    type = in;
+}
+AFilter::~AFilter() {
+    std::cout << "~AFilter(D222) \n";
+}
+void AFilter::start() {
+    std::cout << "start(D222):" << "type=" << type <<"\n";
+}
+
+int main()
+{
+    Filter* aFilter = new AFilter(222);
+    aFilter->start();
+    
+    Filter* filter = new Filter(111);
+    filter->start();
+    delete aFilter;
+    delete filter;
+}
